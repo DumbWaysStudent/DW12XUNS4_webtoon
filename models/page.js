e62ec0e21628520394ba9778b57e5e44dc51ca51
teukurijal <teukurijal89@gmail.com>
2019-10-16 13:12:49 +0700

    delete my episode implementation

---
+++ models/page.js
@@ -0,0 +1,12 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const page = sequelize.define('page', {
+    page: DataTypes.STRING,
+    image: DataTypes.STRING,
+    episodeId: DataTypes.INTEGER
+  }, {});
+  page.associate = function(models) {
+    // associations can be defined here
+  };
+  return page;
+};
